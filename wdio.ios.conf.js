79bc107273d95cb7c1792c2c7e588f3a1d51ee88
Hellen Taís <taistj3@gmail.com>
2025-07-21 16:53:51 -0400

    test mobile ios

---
+++ wdio.ios.conf.js
@@ -0,0 +1,66 @@
+export const config = {
+
+    //runner: 'local',
+    //port: 4723, 
+
+    user: 'oauth-taistj3-833b9',
+    key: '6a6fdde0-cfee-46aa-9f62-4d948e00bda3',
+    hostname: 'ondemand.us-west-1.saucelabs.com',
+    port: 443,
+    baseUrl: 'wd/hub',
+
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    maxInstances: 1,
+
+    capabilities: [
+        {
+            platformName: "iOS",
+            "appium:automationName": "XCUITest",
+            "appium:platformVersion": "17.0",
+            "appium:app": "storage:filename=LojaEBAC-sim.zip",
+            "appium:deviceName": "iPhone Simulator",
+            "appium:disableIdLocatorAutocompletion": true,
+            "sauce:options": {
+                build: "ios-build-lojaEbac",
+                name: "test-ios-lojaEbac",
+                appiumVersion: "2.0.0"
+
+            },
+
+            /*platformName: 'Android',
+            'appium:deviceName': 'ebac-android9',
+            'appium:platformVersion': '9.0',
+            'appium:automationName': 'UiAutomator2',
+            'appium:app': `${process.cwd()}/app/ebacshop.apks`,
+            'appium:appWaitActivity': '.MainActivity',
+            'appium:disableIdLocatorAutocompletion': true*/
+        }],
+
+    logLevel: 'info',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 300000, // 5 minutos
+    connectionRetryCount: 3,
+    framework: 'mocha',
+
+    reporters: ['spec', ['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+    }]],
+
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+
+
+}
+
+/*afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+    if (test) {
+        await driver.takeScreenshot();
+    }
+*/
